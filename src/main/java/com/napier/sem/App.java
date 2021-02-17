ffe95d418844ed8eba7b69d27eb3b7fd4460de12
Colin <colin.edmiston@googlemail.com>
2021-02-17 11:17:58 +0000

    Updated with first working query working.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -8,7 +8,7 @@ public class App
     {
         // Create new Application
         App a = new App();
-
+        System.out.println("This should work");
         // Connect to database
         a.connect();
         // Get Employee
@@ -34,7 +34,7 @@ public class App
         try
         {
             // Load Database driver
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         }
         catch (ClassNotFoundException e)
         {
@@ -78,6 +78,7 @@ public class App
             {
                 // Close connection
                 con.close();
+                System.out.println("Connection closed");
             }
             catch (Exception e)
             {
@@ -93,23 +94,32 @@ public class App
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
-            String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
+            String strSelect = "SELECT e1.emp_no, e1.first_name, e1.last_name, titles.title, salaries.salary, " +
+                    "dp1.dept_name, e2.first_name as manager_firstname, e2.last_name as manager_lastname " +
+                    "FROM employees e1 JOIN titles ON titles.emp_no = e1.emp_no " +
+                    "JOIN dept_emp ON dept_emp.emp_no = e1.emp_no " +
+                    "JOIN departments dp1 ON dp1.dept_no = dept_emp.dept_no " +
+                    "JOIN dept_manager dm1 ON dm1.dept_no = dp1.dept_no " +
+                    "JOIN salaries ON salaries.emp_no = e1.emp_no JOIN employees e2 ON e2.emp_no IN " +
+                    "(SELECT dm2.emp_no FROM dept_manager dm2 WHERE dm2.dept_no = dp1.dept_no AND dm2.to_date = '9999-01-01') " +
+                    "WHERE dept_emp.emp_no = '" + ID + "' AND salaries.to_date = '9999-1-1' AND dm1.to_date = '9999-1-1' " +
+                    "AND titles.to_date = '9999-1-1' AND dept_emp.to_date = '9999-1-1';" ;
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
             // Check one is returned
-            if (rset.next())
-            {
+            if (rset.next()) {
                 Employee emp = new Employee();
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
+                emp.title = rset.getString("titles.title");
+                emp.salary = rset.getInt("salaries.salary");
+                emp.dept_name = rset.getString("dp1.dept_name");
+                emp.manager = rset.getString("manager_firstname") + " " + rset.getString("manager_lastname");
+                ;
                 return emp;
-            }
-            else
+            } else
                 return null;
         }
         catch (Exception e)
